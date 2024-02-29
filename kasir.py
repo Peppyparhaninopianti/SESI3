0bbb7b88539df915ada62c380a88d1b29e12e99e
Peppyparhaninopianti <160814944+Peppyparhaninopianti@users.noreply.github.com>
2024-02-29 11:25:46 +0700

    Add files via upload

---
+++ kasir.py
@@ -0,0 +1,15 @@
+#ucup membeli barang ke warung
+rokok = 20000
+minyak = 25000
+gula = 11000
+saldo = 100000
+diskon = 10000
+
+totalBelanja = rokok + minyak + gula
+totalDiskon = totalBelanja-diskon
+kembalian = saldo-totalDiskon
+
+print ("total jumlah belanja = ", rokok + minyak + gula)
+print ("jumlah diskon =", diskon)
+print ("saldo = ", saldo)
+print ("kembalian =", kembalian )
